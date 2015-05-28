3630e4f2e9a33c1e4b8f527bafe97a0611965397
WhirlingWind <computeros@naver.com>
2015-05-28 23:43:26 +0900

    Activity Revision
    Activity revision

--- HangulKeyboard/src/org/kandroid/app/hangulkeyboard/Setting.java
+++ HangulKeyboard/src/org/kandroid/app/hangulkeyboard/Setting.java
@@ -4,20 +4,18 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 public class Setting extends Activity {
-	
+
+	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.setting);
 		
-		LinearLayout mainLayout = new LinearLayout(this);
-		mainLayout.setBackgroundColor(Color.parseColor("#ffffff"));
+		LinearLayout update = (LinearLayout)findViewById(R.id.data_update);
+		LinearLayout clear = (LinearLayout)findViewById(R.id.clear_data);
+		LinearLayout key_arr = (LinearLayout)findViewById(R.id.key_arrangement);
+		//update.setOnClickListener();
 		
-		TextView txtView = new TextView(this);
-		txtView.setText("First one");
-		mainLayout.addView(txtView);
-		
-		setContentView(mainLayout);
 	}
 }
